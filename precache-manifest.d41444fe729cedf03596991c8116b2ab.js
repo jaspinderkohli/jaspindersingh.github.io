7fbfa47aee54f94e74605a909a113066a9b8a159
Jaspinder Singh <jaspinderkohli@gmail.com>
2023-01-14 04:03:52 -0330

    Deploy React app to GitHub Pages

---
+++ precache-manifest.d41444fe729cedf03596991c8116b2ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd594600aa010c195fb35e569571b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2485556b0091f7ab65f0",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1f5f744e3f6acf317ba2",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "2485556b0091f7ab65f0",
+    "url": "/static/js/2.f2334e63.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.f2334e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5f744e3f6acf317ba2",
+    "url": "/static/js/main.078da2eb.chunk.js"
+  },
+  {
+    "revision": "bc0acab3bb9ed59355ff",
+    "url": "/static/js/runtime-main.41cf8128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-h/precache-manifest.0518dce225924a12981a784601896b0b.js"
+  "/precache-manifest.d41444fe729cedf03596991c8116b2ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-h/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
